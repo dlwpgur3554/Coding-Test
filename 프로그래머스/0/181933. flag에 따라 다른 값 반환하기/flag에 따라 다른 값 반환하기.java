affee086509a05f74512ecaf4fecb036a43d56a1
dlwpgur3554 <dlwpgur3554@naver.com>
2025-04-16 11:51:43 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 89.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b, boolean flag) {
+        if(flag == true){
+            return a + b;
+        }
+        else if(flag == false){
+            return a - b;
+        }
+        return 0;
+    }
+}
