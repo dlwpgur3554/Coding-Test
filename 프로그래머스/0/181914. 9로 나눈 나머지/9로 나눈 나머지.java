c0d89d27cdf37dc8547252e816e13bc50528f0c3
dlwpgur3554 <dlwpgur3554@naver.com>
2025-05-29 20:20:16 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.73 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public static int solution(String number) {
+        int sum = 0;
+        for (int i = 0; i < number.length(); i++) {
+            sum += number.charAt(i) - '0';
+        }
+        return sum % 9;
+    }
+}
