a297cde711f61850cac66efc2c23c2fbe43dff67
dlwpgur3554 <dlwpgur3554@naver.com>
2025-06-12 23:17:18 +0900

    [level 2] Title: 완전범죄, Time: 0.48 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/389480. 완전범죄/완전범죄.java
+++ 프로그래머스/2/389480. 완전범죄/완전범죄.java
@@ -1,39 +1,44 @@
-import java.util.Arrays;
-
 public class Solution {
-    public int solution(int[][] info, int n, int m) {
+    public static int solution(int[][] info, int n, int m) {
         int itemCount = info.length;
         int INF = 999999;
+        
+        // dp[i][j] = i번째 물건까지 사용했을 때 B 흔적 j일 때의 A 도둑 최소 흔적
         int[][] dp = new int[itemCount + 1][m];
         
         for (int i = 0; i <= itemCount; i++) {
-            Arrays.fill(dp[i], INF);
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = INF;
+            }
         }
-        dp[0][0] = 0;
+        dp[0][0] = 0; // 초기 상태
 
         for (int i = 0; i < itemCount; i++) {
             int aTrace = info[i][0];
             int bTrace = info[i][1];
+
             for (int j = 0; j < m; j++) {
                 if (dp[i][j] == INF) continue;
 
-                int nextATrace = dp[i][j] + aTrace;
-                if (nextATrace < n) {
-                    dp[i + 1][j] = Math.min(dp[i + 1][j], nextATrace);
+                // A 도둑이 훔치는 경우
+                int aSum = dp[i][j] + aTrace;
+                if (aSum < n) {
+                    dp[i + 1][j] = Math.min(dp[i + 1][j], aSum);
                 }
 
-                int nextBTrace = j + bTrace;
-                if (nextBTrace < m) {
-                    dp[i + 1][nextBTrace] = Math.min(dp[i + 1][nextBTrace], dp[i][j]);
+                // B 도둑이 훔치는 경우
+                int bSum = j + bTrace;
+                if (bSum < m) {
+                    dp[i + 1][bSum] = Math.min(dp[i + 1][bSum], dp[i][j]);
                 }
             }
         }
 
-        int result = INF;
+        int minA = INF;
         for (int j = 0; j < m; j++) {
-            result = Math.min(result, dp[itemCount][j]);
+            minA = Math.min(minA, dp[itemCount][j]);
         }
 
-        return result == INF ? -1 : result;
+        return minA == INF ? -1 : minA;
     }
 }
